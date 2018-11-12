6d5729b993ed014cb3a9c7e507b1df31a7f0418c
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2018-11-12 10:41:21 +0100

    added resolver

---
+++ angular-src/src/app/resolvers/users.resolver.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+
+import { DataRetrieverService } from '../services/data-retriever.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable()
+export class UsersResolver implements Resolve<User[]> {
+
+  constructor(private dbRetrieverService: DataRetrieverService) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<any> {
+      
+    return this.dbRetrieverService.getAllUsers();
+
+  }
+
+}

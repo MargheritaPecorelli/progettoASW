6853cef61e0c13261002cf63977b702ba4bfa701
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2018-11-14 13:36:58 +0100

    edited date working

--- angular-src/src/app/calendar-selector/calendar-selector.component.ts
+++ angular-src/src/app/calendar-selector/calendar-selector.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter , Input } from '@angular/core';
 import { NgxDateRangePickerOptions, NgxDateRangePickerDates } from 'ngx-daterangepicker';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'calendar-selector',
@@ -12,6 +13,7 @@ export class CalendarSelectorComponent {
   options: NgxDateRangePickerOptions;
   value: NgxDateRangePickerDates;
   @Output() dateChangeEventEmitter: EventEmitter<String>;
+  @Input() updateDate: Subject<string>;
 
   constructor() {
     this.dateChangeEventEmitter = new EventEmitter();
@@ -19,34 +21,49 @@ export class CalendarSelectorComponent {
 
   emitDateChangeEvent() {
     var dateRange = this.value.from.toString() + "T00:00:00Z;" + this.value.to.toString() + "T23:59:00Z";
-    // console.log(dateRange);
+    console.log(dateRange);
     this.dateChangeEventEmitter.emit(dateRange);
   }
 
+  // resetDate() {
+  //   var data  = new Date()
+  //   var dateValue = {year: data.getFullYear(), month: data.getMonth()+1, day: data.getDate() }
+  //   this.options.date.from = dateValue;
+  //   this.options.date.to = dateValue;
+  //   this.value.from = dateValue;
+  //   this.value.to = dateValue;
+  // }
+
   ngOnInit() {
     this.options = {
-            theme: 'green',
-            labels: ['Start', 'End'],
-            menu: [
-                {alias: 'td', text: 'Today', operation: '0d'},
-                {alias: 'tm', text: 'This Month', operation: '0m'},
-                {alias: 'lm', text: 'Last Month', operation: '-1m'},
-                {alias: 'tw', text: 'This Week', operation: '0w'},
-                {alias: 'lw', text: 'Last Week', operation: '-1w'},
-                {alias: 'ty', text: 'This Year', operation: '0y'},
-                {alias: 'ly', text: 'Last Year', operation: '-1y'},
-                {alias: 'ny', text: 'Next Year', operation: '+1y'},
-                {alias: 'lyt', text: 'Last year from today', operation: '-1yt'},
-            ],
-            dateFormat: 'YYYY-MM-DD',
-            outputFormat: 'YYYY-MM-DD',
-            startOfWeek: 0,
-            outputType: 'object',
-            locale: 'en-UK',
-            date: {
-                from: new Date(),
-                to: new Date()
-            }
-        };
+      theme: 'green',
+      labels: ['Start', 'End'],
+      menu: [
+          {alias: 'td', text: 'Today', operation: '0d'},
+          {alias: 'tm', text: 'This Month', operation: '0m'},
+          {alias: 'lm', text: 'Last Month', operation: '-1m'},
+          {alias: 'tw', text: 'This Week', operation: '0w'},
+          {alias: 'lw', text: 'Last Week', operation: '-1w'},
+          {alias: 'ty', text: 'This Year', operation: '0y'},
+          {alias: 'ly', text: 'Last Year', operation: '-1y'},
+          {alias: 'lyt', text: 'Last year from today', operation: '-1yt'},
+      ],
+      dateFormat: 'YYYY-MM-DD',
+      outputFormat: 'YYYY-MM-DD',
+      startOfWeek: 0,
+      outputType: 'object',
+      locale: 'en-UK',
+      date: {
+          from: new Date(),
+          to: new Date()
+      }
+    };
+    // this.updateDate.subscribe(param => {
+    //   console.log("dateChange")
+    //   console.log(this.options.date);
+    //   this.resetDate();
+    //   console.log(this.options.date);
+    // });
+
   }
 }
